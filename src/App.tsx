2ca8b31c0966fba2da756c64d2138a2ebbf16d94
huhu <p.kiani@live.com>
2024-10-22 13:04:37 +0330

    add ComboBox to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useState } from 'react'
 import './App.css'
+import ComboBox from './assets/components/combobox/ComboBox'
 
 function App() {
-  return <>App</>
+  const [options, setOptions] = useState<string[]>([
+    'test1',
+    'test2',
+    'test3',
+    'test4',
+    'test5',
+    'test6',
+    'test7',
+    'test8',
+  ])
+
+  const onAddOption = (value: string) => {
+    setOptions((prev) => Array.from(new Set(prev).add(value)))
+  }
+  return (
+    <>
+      <ComboBox name="test-input" {...{ options, onAddOption }} />
+    </>
+  )
 }
 
 export default App
